9495fd99a0e186d4596b238b6c5d97487bac1dfe
Laura Slingerland <laura.sling@gmail.com>
2017-04-24 22:56:13 -0600

    Assignment 21 jQuery Seek Bars: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -34,7 +34,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
       + '</tr>'
       ;
  
@@ -131,6 +131,7 @@ var updateSeekBarWhileSongPlays = function() {
              var $seekBar = $('.seek-control .seek-bar');
  
              updateSeekPercentage($seekBar, seekBarFillRatio);
+             setCurrentTimeInPlayerBar(this.getTime());
          });
      }
  };
@@ -177,6 +178,7 @@ var setupSeekBars = function() {
              }
  
              updateSeekPercentage($seekBar, seekBarFillRatio);
+             setCurrentTimeInPlayerBar(seekBarFillRatio);
          });
  
          $(document).bind('mouseup.thumb', function() {
@@ -195,8 +197,8 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
-
     $('.main-controls .play-pause').html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
 };
 
 var nextSong = function() {
@@ -246,6 +248,23 @@ var previousSong = function() {
     $previousSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
 };
+
+var setCurrentTimeInPlayerBar = function(currentTime){  
+     $(".current-time").text(filterTimeCode(currentTime));
+ };
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+    $(".total-time").text(filterTimeCode(totalTime));
+ };
+
+var filterTimeCode = function (timeInSeconds) {
+     var float = parseFloat(timeInSeconds);
+     var minutes = Math.floor(float / 60);
+     var seconds = Math.floor(float - minutes * 60);
+
+     return (minutes + ":" + seconds);
+ };
+
  
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
